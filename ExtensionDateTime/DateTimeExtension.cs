d4c447ab1ed68252c7cdb52ff080741d38665019
Anna Tassinari <ANNA.TASSINARI@local.ittcesena.it>
2025-10-06 08:59:39 +0200

    started first method uhh bye

---
+++ ExtensionDateTime/DateTimeExtension.cs
@@ -0,0 +1,22 @@
+﻿namespace ExtensionDateTime
+{
+    public static class DateTimeExtension
+    {
+        public static Seasons GetSeason(this DateTime date)
+        {
+            Seasons season = new Seasons();
+            if(date.DayOfYear >= 80 || date.DayOfYear <=171)
+            {
+                season = Seasons.SPRING;
+            }
+            else if (date.DayOfYear >= 172 ||  date.DayOfYear <= 263)
+            {
+                season = Seasons.SUMMER;
+            }
+            else if (date.DayOfYear >= 264 || date.DayOfYear <= 355)
+            {
+                season = Seasons.AUTUMN;
+            }
+        }
+    }
+}
